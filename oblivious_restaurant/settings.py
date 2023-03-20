226c42285d7a2aa1e10ad293878e4be79059c4ca
JoseParra28 <iliftandcook@gmail.com>
2023-03-20 23:31:02 +0000

    create first view - index

--- oblivious_restaurant/settings.py
+++ oblivious_restaurant/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'food'
 ]
 
 MIDDLEWARE = [
@@ -117,4 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+STATICFILE_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+STATICFILE_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
--- oblivious_restaurant/urls.py
+++ oblivious_restaurant/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from food.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name="index")
 ]
