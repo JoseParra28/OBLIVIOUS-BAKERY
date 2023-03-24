cde605587b69d8fae13aeb175af869b7333db1eb
JoseParra28 <iliftandcook@gmail.com>
2023-03-24 05:59:13 +0000

    create super user

--- food/models.py
+++ food/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Pie(models.Model):
+    name = models.CharField(max_length=120)
+    price_m = models.CharField(max_length=5)
+    price_l = models.CharField(max_length=5)
+    p_image = models.URLField()
+
+
+
