281acf961daa734e5008f75e68341e96a8f47cda
JoseParra28 <iliftandcook@gmail.com>
2023-03-27 07:03:29 +0000

    add the name value when adding a pie

--- food/static/javascript/orders.js
+++ food/static/javascript/orders.js
@@ -4,5 +4,7 @@ const pTotal = document.querySelector('#p-total');
 // add pie function
 function addPie(pie){
     // get a pie 
-    pCart.innerHTML += '<li>' + pie + '</li>';
+    pieId = '#pie-id' + pie;
+    const name = document.querySelector(pieId).innerHTML;
+    pCart.innerHTML += '<li>' + name + '</li>';
 }
