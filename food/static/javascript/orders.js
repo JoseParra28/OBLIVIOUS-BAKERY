f6e9043c54eb25298b6338263765a2f806fcb0ac
JoseParra28 <iliftandcook@gmail.com>
2023-03-27 07:55:58 +0000

    add price info when user selects an item

--- food/static/javascript/orders.js
+++ food/static/javascript/orders.js
@@ -3,8 +3,22 @@ const pTotal = document.querySelector('#p-total');
 
 // add pie function
 function addPie(pie){
-    // get a pie 
+    //  pie name info 
     pieId = '#pie-id' + pie;
-    const name = document.querySelector(pieId).innerHTML;
-    pCart.innerHTML += '<li>' + name + '</li>';
+    let name = document.querySelector(pieId).innerHTML;
+    // pie price info + select specific item
+    let radio = 'pie-rad' + pie;
+    let pric = document.getElementsByName(radio);
+    let size, price ;
+    if (pric[0].checked){
+    price = pric[0].value;
+    size = 'M';
+    }
+    else {
+        price = pric[1].value;
+        size = 'L'
+    }
+
+    pCart.innerHTML += '<li>' + name + ' ' + size + ' : ' + price + '</li>';
+    
 }
