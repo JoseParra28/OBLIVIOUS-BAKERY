eba3eb086d8c9c5e1abd59a6a69023fdd2d65b92
JoseParra28 <iliftandcook@gmail.com>
2023-03-27 06:56:46 +0000

    create add a pie function

--- food/static/javascript/orders.js
+++ food/static/javascript/orders.js
@@ -1,1 +1,8 @@
+const pCart = document.querySelector('#p-cart');
+const pTotal = document.querySelector('#p-total');
 
+// add pie function
+function addPie(pie){
+    // get a pie 
+    pCart.innerHTML += '<li>' + pie + '</li>';
+}
