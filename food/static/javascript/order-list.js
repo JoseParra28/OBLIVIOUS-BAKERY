ccd82a70888195948b96e30105caef36014687af
JoseParra28 <iliftandcook@gmail.com>
2023-03-30 18:04:36 +0000

    add remove item from on order list html

--- food/static/javascript/order-list.js
+++ food/static/javascript/order-list.js
@@ -1,52 +1,52 @@
-let billName = document.querySelector('#ord-name')
-let size = document.querySelector('#ord-size')
-let price = document.querySelector('#ord-price')
-let totalBill = document.querySelector('#total-bill')
-let orderBtn = document.querySelector('#remove-item-order')
+// let billName = document.querySelector('#ord-name')
+// let size = document.querySelector('#ord-size')
+// let price = document.querySelector('#ord-price')
+// let totalBill = document.querySelector('#total-bill')
+// let orderBtn = document.querySelector('#remove-item-order')
 
 
 
-function orderList() {
-    let  orders = JSON.parse(localStorage.getItem('orders'));
-    let  total = localStorage.getItem('total');
-    let cartSize = orders .length;
+// function orderList() {
+//     let  orders = JSON.parse(localStorage.getItem('orders'));
+//     let  total = localStorage.getItem('total');
+//     let cartSize = orders .length;
 
-    billName.innerHTML = '<h3>name</h3>';
-    size.innerHTML = '<h3>size</h3>';
-    price.innerHTML = '<h3>price</h3>';
-    orderBtn.innerHTML = '<h3><br></h3>';
+//     billName.innerHTML = '<h3>name</h3>';
+//     size.innerHTML = '<h3>size</h3>';
+//     price.innerHTML = '<h3>price</h3>';
+//     orderBtn.innerHTML = '<h3><br></h3>';
    
  
 
-    for (let i = 0; i < cartSize; i++ ){
-        orderBtn.innerHTML  += '<h4><button> class="btn-danger" onclick="removeItem(' + i + ')" style="position:relative;>Remove</button></h4>';
-        billName.innerHTML += '<h4>'+ orders[i][0] +'</h4>'
-        size.innerHTML += '<h4>'+ orders[i][1] +'</h4>'
-        price.innerHTML += '<h4>'+ orders[i][2] +'</h4>'
+//     for (let i = 0; i < cartSize; i++ ){
+//         orderBtn.innerHTML  += '<h4><button> class="btn-danger" onclick="removeItem(' + i + ')" style="position:relative;>Remove</button></h4>';
+//         billName.innerHTML += '<h4>'+ orders[i][0] +'</h4>'
+//         size.innerHTML += '<h4>'+ orders[i][1] +'</h4>'
+//         price.innerHTML += '<h4>'+ orders[i][2] +'</h4>'
 
 
-        pCart.innerHTML += '<li>' + orders[i][0] + ' ' + orders[i][1] + ' : ' + orders[i][2] + btn + '</li>';
-    }
-    totalBill.innerHTML = '<h3>order total</h3>' + total + ' £';
+//         pCart.innerHTML += '<li>' + orders[i][0] + ' ' + orders[i][1] + ' : ' + orders[i][2] + btn + '</li>';
+//     }
+//     totalBill.innerHTML = '<h3>order total</h3>' + total + ' £';
     
-    orderList();
-}
+//     orderList();
+// }
 
-let note = document.querySelector('#message')
+// let note = document.querySelector('#message')
 
-function submitOrder(){
-    let msg = note.value;
-    let orders = localStorage.getItem('orders');
-    orderData['orders'] = orders;
-    let ur = 'food/order-list';
-    let orderData = {};
-    orderData['note'] = msg;
-    $.ajax({
-        url: ur,
-        type: "POST",
-        data: orderData,
-        success: function(data){
-            console.log('the data was sent');
-        }
-    })
-}
+// function submitOrder(){
+//     let msg = note.value;
+//     let orders = localStorage.getItem('orders');
+//     orderData['orders'] = orders;
+//     let ur = 'food/order-list';
+//     let orderData = {};
+//     orderData['note'] = msg;
+//     $.ajax({
+//         url: ur,
+//         type: "POST",
+//         data: orderData,
+//         success: function(data){
+//             console.log('the data was sent');
+//         }
+//     })
+// }
---
+++ food/static/javascript/shopping-cart.js
@@ -0,0 +1,43 @@
+let billName = document.querySelector('#ord-name')
+let size = document.querySelector('#ord-size')
+let price = document.querySelector('#ord-price')
+let totalBill = document.querySelector('#total-bill')
+let rm = document.querySelector('#rm')
+
+function shoppingCartTotal() {
+    let  orders = JSON.parse(localStorage.getItem('orders'));
+    let  total = localStorage.getItem('total');
+    let cartSize = orders .length;
+
+    billName.innerHTML = '<h3>name</h3>';
+    size.innerHTML = '<h3>size</h3>';
+    price.innerHTML = '<h3>price</h3>';
+    rm.innerHTML = '<h3>Remove</h3>';
+    
+
+    for (let i = 0; i < cartSize; i++ ){
+        rm.innerHTML += '<h4><button class="btn-danger" onclick="removeItem(' + i + ')">X</button></h4>';
+        billName.innerHTML += '<h4>' + orders[i][0] + '</h4>';
+        size.innerHTML += '<h4>' + orders[i][1] + '</h4>';
+        price.innerHTML += '<h4>' + orders[i][2] + '</h4>';
+    }
+    totalBill.innerHTML = 'Total ' + total + ' £';
+}
+shoppingCartTotal();
+
+function removeItem(n){
+    let  orders = JSON.parse(localStorage.getItem('orders'));
+    let  total = localStorage.getItem('total');
+
+    total = Number(total) - Number(orders[n][2]);
+    orders.splice(n,1);
+
+     // updating order 
+     const trolley = document.querySelector('#trolley');
+     trolley.innerHTML = orders.length;
+
+    localStorage.setItem('orders', JSON.stringify(orders));
+    localStorage.setItem('total', total);
+
+    shoppingCartTotal();
+}
