673d2da9b5f2d9b875c22694e1dd3570854d89b1
JoseParra28 <iliftandcook@gmail.com>
2023-03-31 09:57:36 +0000

    create 2 models, Order and Item to add order to the database

--- food/models.py
+++ food/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.contrib.auth.models import User
 
 
 class Pie(models.Model):
@@ -24,6 +25,20 @@ class Donut(models.Model):
     price_l = models.CharField(max_length=5)
     p_image = models.URLField()    
 
+
+class Order (models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    number = models.CharField(max_length=50)
+    receipt = models.DecimalField(max_digits=4, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+    notes = models.TextField(blank=True, null=True)
+
+
+class Item(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    price = models.CharField(max_length=5)
+    size = models.CharField(max_length=5)
 
 
 
