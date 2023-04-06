b77043941553e280b83b1659cf6625fc7d6323b5
JoseParra28 <iliftandcook@gmail.com>
2023-04-06 09:47:10 +0000

    3 tests passed susscesfully

--- food/tests.py
+++ food/tests.py
@@ -1,7 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Pie, Cake
 
 
 class indexTest(TestCase):
     def test_index(self):
-        self.assertEqual('test1', 'test1')
-        
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+
+class pieTest(TestCase):
+    def test_newPie_added(self):
+        numPie = Pie.objects.count()
+        Pie.objects.create(name='pie2', price_m=34, price_l=59, p_image="someUrl")
+        self.assertEqual(Pie.objects.count(), numPie+1)
+
+
+class cakeTest(TestCase):
+    def test_newCake_added(self):
+        numCake = Cake.objects.count()
+        Cake.objects.create(name='cake2', price_m=34, price_l=59, p_image="someUrl")
+        self.assertEqual(Cake.objects.count(), numCake+1)      
