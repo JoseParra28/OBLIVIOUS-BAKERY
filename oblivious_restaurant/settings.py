9b4b77add46d21459e9f0f83013809341e302c87
JoseParra28 <iliftandcook@gmail.com>
2023-04-19 07:19:46 +0000

    edit settings

--- oblivious_restaurant/settings.py
+++ oblivious_restaurant/settings.py
@@ -137,7 +137,7 @@ STATICFILE_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILE_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/images/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
