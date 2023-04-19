4e40992044983df6a828b7f2d24f9b59bd3464b5
JoseParra28 <iliftandcook@gmail.com>
2023-04-19 08:12:32 +0000

    edit settings

--- oblivious_restaurant/settings.py
+++ oblivious_restaurant/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import food
 from django.contrib.messages import constants as messages
-import cloudinary
+# import cloudinary
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'cloudinary_storage',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'food',
@@ -143,7 +143,7 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = '/static/'
-# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
